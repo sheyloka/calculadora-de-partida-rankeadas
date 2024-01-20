db653d5cee4df9a56108c2b477037792845bc4c8
Sheyla <sheylaalmeid@gmail.com>
2024-01-20 18:49:12 -0300

    novo projeto

---
+++ index.js
@@ -0,0 +1,26 @@
+calcularNivel(50, 2);
+
+function calcularNivel(vitorias, derrotas) {
+
+    const saldoVitorias = vitorias - derrotas;
+
+    let nivel;
+
+    if (vitorias < 10) {
+        nivel = "Ferro";
+    } else if (vitorias <= 20) {
+        nivel = "Bronze";
+    } else if (vitorias <= 50) {
+        nivel = "Prata";
+    } else if (vitorias <= 80) {
+        nivel = "Ouro";
+    } else if (vitorias <= 90) {
+        nivel = "Diamante";
+    } else if (vitorias <= 100) {
+        nivel = "Lendário";
+    } else {
+        nivel = "Imortal";
+    }
+
+    console.log(`O Herói tem um saldo de ${saldoVitorias} e está no nível de ${nivel}`);
+}
