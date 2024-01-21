8a4dcc2e1646d7d006d8befbebc856afe1572440
Sheyla <sheylaalmeid@gmail.com>
2024-01-21 17:31:33 -0300

    Projeto finalizado

--- index.js
+++ index.js
@@ -6,20 +6,27 @@ function calcularNivel(vitorias, derrotas) {
 
     let nivel;
 
-    if (vitorias < 10) {
-        nivel = "Ferro";
-    } else if (vitorias <= 20) {
-        nivel = "Bronze";
-    } else if (vitorias <= 50) {
-        nivel = "Prata";
-    } else if (vitorias <= 80) {
-        nivel = "Ouro";
-    } else if (vitorias <= 90) {
-        nivel = "Diamante";
-    } else if (vitorias <= 100) {
-        nivel = "Lendário";
-    } else {
-        nivel = "Imortal";
+    switch (true) {
+        case vitorias < 10:
+            nivel = "Ferro";
+            break;
+        case vitorias <= 20:
+            nivel = "Bronze";
+            break;
+        case vitorias <= 50:
+            nivel = "Prata";
+            break;
+        case vitorias <= 80:
+            nivel = "Ouro";
+            break;
+        case vitorias <= 90:
+            nivel = "Diamante";
+            break;
+        case vitorias <= 100:
+            nivel = "Lendário";
+            break;
+        default:
+            nivel = "Imortal";
     }
 
     console.log(`O Herói tem um saldo de ${saldoTotal} e está no nível de ${nivel}`);
